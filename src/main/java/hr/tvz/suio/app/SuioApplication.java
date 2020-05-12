aeab0d7f0d1c4ec65a74eae7d8565cc53d73983a
szivkovic12 <stjzivkovic93@gmail.com>
2020-05-12 16:41:19 +0200

    first commit

---
+++ src/main/java/hr/tvz/suio/app/SuioApplication.java
@@ -0,0 +1,14 @@
+package hr.tvz.suio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuioApplication.class, args);
+
+	}
+
+}
