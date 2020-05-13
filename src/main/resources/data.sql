ece0bc00547a6c7138852c1bfb1bd62cb0cad7b3
szivkovic12 <stjzivkovic93@gmail.com>
2020-05-13 17:40:50 +0200

    schema i data-new

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,24 @@
+delete from employee_asset;
+delete from asset;
 delete from employee;
+delete from department;
+delete from role;
+delete from type;
 
 insert into employee(id,name,surname,oib,email,date_of_birth,department_id,role_id, date_of_employment)
-values(1,'Stjepan','Zivkovic','70705544333','szivk@tvz.hr',NOW(),1,2, now());
+values(1,'Stjepan','Zivkovic','70705544242','szivk@tvz.hr',NOW(),1,2, NOW());
 
-insert into department(name)
-values ('IT');
+insert into department(id,name)
+values (1,'IT');
 
-insert into role(name)
-values('korisnik');
+insert into role(id,name)
+values(1,'korisnik');
 
-insert into type(name)
-values('laptop');
+insert into asset(id,name,serial_number,product_number,type_id,date_of_purchase, inventory_number,status,date_of_disposal)
+values(1,'LENOVO','123456','654321', 1, NOW(),'111554','1', NOW());
 
-insert into asset(name,serial_number,product_number,type_id,date_of_purchase, inventory_number,status,date_of_disposal)
-values('LENOVO','123456','654321', 1, NOW(),'111554','1', NOW());
+insert into type(id,name)
+values(1,'laptop');
 
 insert into employee_asset(employee_id,asset_id)
 values (1,1);
-
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,11 +23,6 @@ id identity auto_increment,
 name varchar(50) not null
 );
 
-create table if not exists type (
-id identity auto_increment,
-name varchar(50) not null
-);
-
 create table if not exists asset (
 id identity auto_increment,
 name varchar(100) not null,
@@ -40,6 +35,20 @@ status varchar(20) not null,
 date_of_disposal date not null,
 constraint fk_type foreign key (type_id) references type(id)
 );
+
+create table if not exists type (
+id identity auto_increment,
+name varchar(50) not null
+);
+
+create table if not exists employee_asset (
+    id identity auto_increment,
+    employee_id bigint,
+    asset_id bigint,
+    constraint fk_student foreign key (employee_id) references employee(id),
+    constraint fk_course foreign key (asset_id) references asset(id)
+);
+
 
 
 create table if not exists employee_asset (
