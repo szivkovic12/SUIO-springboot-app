d7531a35d8e041dd82fd96cc2a8fc00ab32df6e2
szivkovic12 <stjzivkovic93@gmail.com>
2020-05-13 20:20:21 +0200

    packages

---
+++ src/main/java/hr/tvz/suio/model/Employee.java
@@ -0,0 +1,27 @@
+package hr.tvz.suio.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String surname;
+	private String oib;
+	private String email;
+	private Date date_of_birth;
+	private Integer department;
+	private Integer role;
+	private Date date_of__employment;
+}
---
+++ src/main/java/hr/tvz/suio/web/EmployeeRestController.java
@@ -0,0 +1,11 @@
+package hr.tvz.suio.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("employee")
+public class EmployeeRestController {
+
+
+}
